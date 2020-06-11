b71cc94d5ae51eca9745f4623f2fc069ffaf8894
kengo0629 <63942635+kengo0629@users.noreply.github.com>
2020-06-11 10:57:50 +0900

    編集/削除/更新機能追加

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
+
+  before_action :set_params, only: [:show, :edit]
   def index
     @jobs = Job.all.order("created_at DESC").limit(6)
   end
@@ -13,10 +15,31 @@ class JobsController < ApplicationController
   end
 
   def show
-    @job = Job.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    job = Job.find(params[:id])
+    job.update(job_params)
+  end
+
+  def destroy
+    job = Job.find(params[:id])
+    job.destroy
   end
 
   def job_params
     params.require(:job).permit(:name, :text, :occupation, :image, :region, :email)
+  end
+
+  private
+  def set_params
+    @job = Job.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
